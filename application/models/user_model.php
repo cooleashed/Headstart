aef4b0e26b5789a2fda15092a5842554b2e8ab77
Karan Desai <kud360@1981106.no-reply.drupal.org>
2012-09-02 14:46:48 +0530

    validate_credentials made

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,11 +1,31 @@
 <?php
 
 class User_model extends CI_model {
+    public function validate_credentials($data)
+    { 
+        $data= array('username'=> $username,'password'=>$password);
     
+    
+        
+       
+        $query = $this->db->get_where('user_deatils',data);
+        $user = $query->result(); 
+        if(count($user)==1) 
+            {
+            return $user[0];
+        }
+ else 
+     {
+     return false;
+      }  
+      }
+      }
     function saveUser($object) {
         $this->db->insert('user_details', $object);
         return $this->db->insert_id();
-    }
+        
+        
+    
     
 }
 ?>
