f386234e903f6f8d049047c478189751914a4f0d
Karan Desai <kud360@1981106.no-reply.drupal.org>
2012-09-02 14:54:15 +0530

    minor change

--- application/models/user_model.php
+++ application/models/user_model.php
@@ -1,32 +1,23 @@
 <?php
 
 class User_model extends CI_model {
-    public function validate_credentials($data)
-    { 
-        $data= array('username'=> $username,'password'=>$password);
-    
-    
-        
-       
-        $query = $this->db->get_where('user_deatils',data);
-        $user = $query->result(); 
-        if(count($user)==1) 
-            {
+
+    public function validate_credentials($data) {
+//        $data = array('username' => $username, 'password' => $password);
+        $query = $this->db->get_where('user_deatils', data);
+        $user = $query->result();
+        if (count($user) == 1) {
             return $user[0];
+        } else {
+            return false;
         }
- else 
-     {
-     return false;
-      }  
-      }
-      }
-    function saveUser($object) {
-        $this->db->insert('user_details', $object);
-        return $this->db->insert_id();
-        
-        
-    
-    
+    }
+
+}
+
+function saveUser($object) {
+    $this->db->insert('user_details', $object);
+    return $this->db->insert_id();
 }
 ?>
 
