3aca9deb69f4dec694c56f51e2455dc30de82e62
shah <shah@shah-PC>
2012-09-02 14:15:24 +0530

    Initial Commit

---
+++ application/views/login.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+    <title>Login</title>
+    <script src="<?php echo base_url() . 'assets/js/bootstrap.min.js' ?>" ></script>
+    <link type="text/stylesheet" src='<?php echo base_url() . "assets/css/bootstrap-responsive.css"; ?>' />
+    <link type="text/stylesheet" src='<?php echo base_url() . "assets/css/bootstrap.min.css"; ?>' />
+    </head>
+    <body>
+        <label>Username:</label>
+        <input type="text" id="user" name="user">
+        <label>Password</label>
+        <input type="password" id="pass" name="pass">
+        <button type="submit" class="btn">
+    </body>
+</html>
